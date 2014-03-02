f76728f2f362eac5a9912f8d5f2cbeddbcb313dd
Ihor Kolodyuk <ik@jelastic.com>
2014-03-02 21:03:40 +0200

    optimization script added

---
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+SED=$(which sed);
+
+#
+# config optimizer for neo4j
+#
+
+CASSANDRA_ENV_FILE="${OPENSHIFT_CASSANDRA_DIR}/versions/1.2.5/conf/cassandra-env.sh";
+
+ [ -z "$XMS" ] && { XMS=32; }
+  memory_total=`free -m | grep Mem | awk '{print $2}'`;
+   [ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}";  }
+
+   $SED -i "s/^MAX_HEAP_SIZE=.*/MAX_HEAP_SIZE=${XMX}/" $CASSANDRA_ENV_FILE;
+
