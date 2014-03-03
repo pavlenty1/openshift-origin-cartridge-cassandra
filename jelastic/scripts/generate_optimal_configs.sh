81cfd688dfd15c5ed2150f331081a3238ca42ce2
Ihor Kolodyuk <ik@jelastic.com>
2014-03-03 15:46:10 +0200

    optimized fixed

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -10,7 +10,7 @@ CASSANDRA_ENV_FILE="${OPENSHIFT_CASSANDRA_DIR}/versions/1.2.5/conf/cassandra-env
 
  [ -z "$XMS" ] && { XMS=32; }
   memory_total=`free -m | grep Mem | awk '{print $2}'`;
-   [ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}";  }
+   [ -z "$XMX" ] && { let "XMX=(memory_total-35)/2"; XMX="${XMX}";  }
 
    $SED -i "s/^MAX_HEAP_SIZE=.*/MAX_HEAP_SIZE=${XMX}/" $CASSANDRA_ENV_FILE;
 
